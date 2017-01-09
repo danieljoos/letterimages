e7deb02eb13ef619a297ed0cd5a4dc3baf36cd64
Daniel Joos <daniel@joosweb.de>
2017-01-09 14:10:18 +0100

    Added build for CSS-only version

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,11 +2,13 @@
 
 'use strict';
 
-const data = require('gulp-data');
+const cheerio = require('cheerio');
+const cssmin = require('gulp-cssmin');
 const es = require('event-stream');
 const gulp = require('gulp');
 const jscs = require('gulp-jscs');
 const jshint = require('gulp-jshint');
+const lodash = require('lodash');
 const material = require('material-colors');
 const path = require('path');
 const reduce = require('stream-reduce');
@@ -15,20 +17,23 @@ const svgo = require('gulp-svgo');
 const template = require('gulp-template');
 const uglify = require('gulp-uglify');
 
-gulp.task('letterimages.js', () =>
+let templateData = { svgs: {}, colors: material };
+
+gulp.task('templateData', () =>
+    gulp.src('svg/*.svg')
+        .pipe(svgo())
+        .pipe(reduce((d, file) => {
+            const k = path.basename(file.path, '.svg');
+            const v = file.contents;
+            d.svgs[k] = v;
+            return d;
+        }, templateData))
+        .on('data', (d) => templateData = d)
+);
+
+gulp.task('letterimages.js', ['templateData'], () =>
     gulp.src('letterimages.js.tmpl')
-        .pipe(data((f, cb) => {
-            gulp.src('svg/*.svg')
-                .pipe(svgo())
-                .pipe(reduce((d, file) => {
-                    const k = path.basename(file.path, '.svg');
-                    const v = file.contents;
-                    d.svgs[k] = v;
-                    return d;
-                }, { svgs: {}, colors: material }))
-                .on('data', (d) => cb(undefined, d));
-        }))
-        .pipe(template())
+        .pipe(template(templateData))
         .pipe(jscs())
         .pipe(jscs.reporter())
         .pipe(jshint())
@@ -41,6 +46,20 @@ gulp.task('letterimages.min.js', ['letterimages.js'], () =>
     gulp.src('dist/letterimages.js')
         .pipe(uglify())
         .pipe(rename('letterimages.min.js'))
+        .pipe(gulp.dest('dist'))
+);
+
+gulp.task('letterimages.css', ['templateData'], () =>
+    gulp.src('letterimages.css.tmpl')
+        .pipe(template(templateData, {imports: { _: lodash, cheerio: cheerio}}))
+        .pipe(rename('letterimages.css'))
+        .pipe(gulp.dest('dist'))
+);
+
+gulp.task('letterimages.min.css', ['letterimages.css'], () =>
+    gulp.src('dist/letterimages.css')
+        .pipe(cssmin())
+        .pipe(rename('letterimages.min.css'))
         .pipe(gulp.dest('dist'))
 );
 
@@ -57,4 +76,7 @@ gulp.task('demo.html', ['letterimages.min.js'], () =>
         .pipe(gulp.dest('dist'))
 );
 
-gulp.task('default', ['letterimages.js', 'letterimages.min.js', 'demo.html']);
+gulp.task('default', [
+    'letterimages.js', 'letterimages.min.js',
+    'letterimages.css', 'letterimages.min.css',
+    'demo.html']);
